2e3bbb2e087ae815141f8e2bbcfb5965f5f02ea2
JGCS2121 <JGCS2121@GMAIL.COM>
2026-05-15 04:24:38 +0200

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -2,16 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-const { transformer, resolver } = config;
-
-config.transformer = {
-  ...transformer,
-  babelTransformerPath: require.resolve('react-native-svg-transformer'),
-};
-config.resolver = {
-  ...resolver,
-  assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
-  sourceExts: [...resolver.sourceExts, 'svg', 'cjs', 'mjs'],
-};
+config.resolver.sourceExts.push('cjs');
+config.resolver.sourceExts.push('mjs');
 
 module.exports = config;
